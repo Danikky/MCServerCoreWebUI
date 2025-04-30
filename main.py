c7b8e493f885792815cadaab355b444ef6167702
Danikky <riperemine@gmail.com>
2025-04-30 23:11:30 +0300

    From All Team

--- main.py
+++ main.py
@@ -8,40 +8,35 @@ app = Flask("__main__")
 def index():
     return render_template("index.html")
 
-# Страница авторизации
-@app.route("/auth")
-def auth():
-    return "/auth"
-
 # Управление (Консоль, Данные, Производительность, Игроки) (Fast data)
 @app.route("/server")
 def server():
-    return "/server"
+    return render_template("server.html")
 
 # Настройка сервера
 @app.route("/server/settings")
 def server_settings():
-    return "/server/settings"
+    return render_template("server_settings.html")
 
 # Управление файлами серрвера (редактирование/создание/удаление файлов, директорий)
 @app.route("/server/files")
 def server_files():
-    return "/server/files"
+    return render_template("server_files.html")
 
 # Управление игроками (Кто играет realtime, Кто заходил, Права, Баны, Вишлист)
 @app.route("/server/players")
 def server_players():
-    return "/server/players"
+    return render_template("server_players.html")
 
 # Управление базами данных (Вывод/редактирование таблиц)
 @app.route("/server/sqltables")
 def server_sql_tables():
-    return "/server/sqltables"
+    return render_template("server_sql.html")
 
 # Карта сервера (Интеграция плагина, Вывод/редактирование)
 @app.route("/server/map")
 def server_map():
-    return "/server/map"
+    return render_template("server_map.html")
 
 # Для безопастного импорта файла(как библиотека) + run
 if __name__ == "__main__":

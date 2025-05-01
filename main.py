25c15a0127b43c8a325a8ce443a76d9c5d029aba
Danikky <riperemine@gmail.com>
2025-05-01 21:01:14 +0300

    Co-authored-by: bibumutirn <bibumutirn@users.noreply.github.com> Co-authored-by: KyryloBaiov <KyryloBaiov@users.noreply.github.com>

--- main.py
+++ main.py
@@ -2,18 +2,13 @@ from flask import Flask, render_template, redirect, url_for, request, flash, jso
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 import sqlite3
 from flask_socketio import SocketIO, emit
-import subprocess
 import os
-import time
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask("__main__")
 app.secret_key = os.urandom(24)
 
 socketio = SocketIO(app)
-
-SERVER_START_CMD = "java -jar server.jar nogui"
-SERVER_PID = None
 
 # Настройка Flask-Login
 login_manager = LoginManager()

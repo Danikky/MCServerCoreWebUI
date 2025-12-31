6592d89a04c855d6cb58c5f312d2fe6ad0b4edcf
Dan1kky <riperemine@gmail.com>
2025-12-31 16:16:29 +0300

    add venv_creater

---
+++ create_venv(fish).sh
@@ -0,0 +1,5 @@
+cd ~/projects/MCServerCoreWebUI/
+python3 -m venv ~/projects/MCServerCoreWebUI/.venv
+source ~/projects/MCServerCoreWebUI/.venv/bin/activate.fish
+pip install -r requirements.txt
+python3 main.py
--- start(linux).sh
+++ start(linux).sh
@@ -1,4 +1,4 @@
 cd ~/projects/MCServerCoreWebUI
-source ~/projects/MCServerCoreWebUI/.venv/bin/activate
+source ~/projects/MCServerCoreWebUI/.venv/bin/activate.fish
 git pull
 python3 main.py
